4a2c05a64b2cfcf41025fe53475d1877f0c7f276
Magesh Kuppan <tkmagesh77@gmail.com>
2019-10-29 16:43:16 +0800

    calcualtor two solution - simplified

--- first-app/src/app/calculator/calculatorTwo.component.ts
+++ first-app/src/app/calculator/calculatorTwo.component.ts
@@ -6,26 +6,5 @@ import { CalculatorModel } from './calculatorModel';
     templateUrl : 'calculatorTwo.component.html'
 })
 export class CalculatorTwoComponent{
-    operator : string = 'add';
     model : CalculatorModel = new CalculatorModel();
-
-    onCalculateClick(){
-        switch (this.operator) {
-            case 'add':
-                this.model.add();
-                break;
-            case 'subtract':
-                this.model.subtract();
-                break;
-            case 'multiply':
-                this.model.multiply();
-                break;
-            case 'divide':
-                this.model.divide();
-                break;
-        
-            default:
-                break;
-        }
-    }
 }
