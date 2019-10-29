b24bb95564de7a8d82b3d995b632bd96ebe61be8
Magesh Kuppan <tkmagesh77@gmail.com>
2019-10-29 15:20:58 +0800

    calculatorOne implementation

---
+++ first-app/src/app/calculator/calculatorModel.ts
@@ -0,0 +1,20 @@
+//state & behavior 
+export class CalculatorModel{
+    //state
+    n1 : number = 0;
+    n2 : number = 0;
+    result : number = 0;
+
+    add(){
+        this.result = this.n1 + this.n2;
+    }
+    subtract(){
+        this.result = this.n1 - this.n2;
+    }
+    multiply(){
+        this.result = this.n1 * this.n2;
+    }
+    divide(){
+        this.result = this.n1 / this.n2;
+    }
+}
---
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CalculatorModel } from './calculatorModel';
+
+@Component({
+    selector : 'app-calculator-1',
+    templateUrl : 'calculatorOne.component.html'
+})
+export class CalculatorOneComponent{
+    model : CalculatorModel = new CalculatorModel();
+}
