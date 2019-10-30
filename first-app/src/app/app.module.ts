b1018d1c2ff73b388ad70baecb8710ab9bbed1f9
Magesh Kuppan <tkmagesh77@gmail.com>
2019-10-30 09:32:24 +0800

    products listing

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { GreeterComponent } from './greeter/greeter.component';
 import { CalculatorOneComponent } from './calculator/calculatorOne.component';
 import { CalculatorTwoComponent } from './calculator/calculatorTwo.component';
 import { CalculatorResultComponent } from './calculator/calculatorResult.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { CalculatorResultComponent } from './calculator/calculatorResult.compone
     , CalculatorOneComponent
     , CalculatorTwoComponent
     , CalculatorResultComponent
+    , ProductsComponent
   ],
   imports: [
     BrowserModule
@@ -23,7 +25,8 @@ import { CalculatorResultComponent } from './calculator/calculatorResult.compone
     AppComponent, 
     GreeterComponent, 
     CalculatorOneComponent, 
-    CalculatorTwoComponent
+    CalculatorTwoComponent,
+    ProductsComponent
   ]
 })
 export class AppModule { }
