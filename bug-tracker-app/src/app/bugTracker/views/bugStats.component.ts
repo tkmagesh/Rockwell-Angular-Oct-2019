ae3f3bf0b533c901fd6e0bbfbc3187e9fe1cd03a
Magesh Kuppan <tkmagesh77@gmail.com>
2019-10-31 12:34:11 +0800

    Introducing 'OnPush' change detection strategy

--- bug-tracker-app/src/app/bugTracker/views/bugStats.component.ts
+++ bug-tracker-app/src/app/bugTracker/views/bugStats.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
 import { Bug } from '../models/Bug';
 
 @Component({
     selector : 'app-bug-stats',
     template : `
+        <h3>{{getDate()}}</h3>
         <section class="stats">
             <span class="closed">{{bugs | closedCount}}</span>
             <span> / </span>
             <span>{{bugs.length}}</span>
         </section>
-    `
+    `,
+    changeDetection : ChangeDetectionStrategy.OnPush
 })
 export class BugStatsComponent{
     @Input()
     bugs : Bug[] = [];
+
+    getDate(){
+        return new Date();
+    }
 }
